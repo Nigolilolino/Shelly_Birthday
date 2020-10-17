e40c8ee6789889429e3bfa57bea85267e8ca3ba3
Nico Klein <34275116+Nigolilolino@users.noreply.github.com>
2020-10-17 18:28:26 +0200

    Add files via upload

--- BirthdaySite/Main.js
+++ BirthdaySite/Main.js
@@ -3,15 +3,23 @@ let jizzposition = 0;
 let strokeNumber = 0;
 let produced = 0;
 let whiteRain = ['img/Jizz_1.png','img/Jizz_2.png','img/Jizz_3.png','img/Jizz_4.png'];
+let jizzExists = false;
 
 window.onload = function () {
     init();
 }
 
 function init(){
+    let cover = document.getElementById("cover_pic");
+    cover.addEventListener('click', removeCover);
+
     let picture = document.getElementById("picture");
-    let button = document.getElementById('clicker');
-    button.addEventListener('click', play);
+    picture.addEventListener('click', play);
+}
+
+function removeCover(){
+    var cover = document.getElementById('cover');
+    cover.parentNode.removeChild(cover);
 }
 
 function play(){
@@ -21,20 +29,22 @@ function play(){
     let spawn = document.getElementById("jizzSpawn");
 
     strokeNumber += 1;
-
-    if((strokeNumber / 10) % 2 == 0){
+    if((strokeNumber / 10) % 2 == 0 && jizzExists == false){
         let image = document.createElement("IMG");
         image.src = "img/Jizz.png";
         image.id = "jizz";
         spawn.appendChild(image);
+        jizzExists = true;
         jizzposition = 0;
     }
     
     if(null != jizz){
         jizzposition = jizzposition - 1;
+        console.log(jizz.style.marginTop);
         jizz.style.marginTop = jizzposition.toString() + "%"; 
-        if(-15 == parseInt(jizz.style.marginTop)){
+        if(-65 == parseInt(jizz.style.marginTop)){
             spawn.removeChild(jizz);
+            jizzExists = false;
             produced += 1;
         }
     }
